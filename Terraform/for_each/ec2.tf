5ee9ab7ba4f8f3f298f14d260becbd746881e92c
Balaji <balaji@eviridis.com>
2026-02-26 20:56:18 +0530

    notes updated

--- Terraform/for_each/ec2.tf
+++ Terraform/for_each/ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "example" {
 
-  for_each = var.instances
+  for_each =toset(var.instances)
 
   ami           = "ami-0220d79f3f480ecf5"
 
-  instance_type = each.value
+  instance_type = "t3.micro"
 
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
