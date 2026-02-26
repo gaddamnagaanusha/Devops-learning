32d7641a8684cc603be37c1a48bd5a70eb1a74a6
Balaji <balaji@eviridis.com>
2026-02-26 20:23:23 +0530

    notes updated

---
+++ Terraform/for_each/variables.tf
@@ -0,0 +1,10 @@
+variable "instances" {
+    type = map
+    default = {
+        mongodb = "t3.micro"
+        mysql = "t3.small"
+        user = "t3.micro"
+        shipping = "t3.small"
+
+    }
+}
