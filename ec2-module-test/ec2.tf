6236779353a59e7cacb44f2146de40dd9274bc0d
Balaji <balaji@eviridis.com>
2026-03-17 16:30:26 +0530

    notes updated

--- ec2-module-test/ec2.tf
+++ ec2-module-test/ec2.tf
@@ -1,3 +1,2 @@
-module "ec2"{    #this is indicating that we are creating a module for ec2 instance, we can give any name to this module
-    source = "/terraform-aws-instance"
-}
+module "ec2" {
+    source = "../terraform-aws-instance"
