b95e388c930a5c115155955885b6cb64b96c5fb1
Balaji <balaji@eviridis.com>
2026-03-17 16:47:40 +0530

    notes updated

--- ec2-module-test/ec2.tf
+++ ec2-module-test/ec2.tf
@@ -1,5 +1,5 @@
-module "ec2" {
+module "ec2" {  
     source = "../terraform-aws-instance"
     ami_id = "ami-0220d79f3f480ecf5"
-    instance_type = "t3.micro"
+    instance_type = "t3.large"
 } 
--- terraform-aws-instance/variables.tf
+++ terraform-aws-instance/variables.tf
@@ -1,9 +1,10 @@
-variable "ami_id" {
+variable "ami_id" { #Here Ami_id is mandatory variable, we have to pass the value of this variable when we are calling this module in other terraform file. This is for making our code reusable and modular. We can use this module for creating multiple ec2 instances with different ami_id and instance_type by just passing the values of these variables.
     type = string
     
 }
 
-variable "instance_type" {
+variable "instance_type" { 
     type = string
+    default = "t3.micro" # this is for default value, if we are not passing the value of this variable when we are calling this module in other terraform file, then it will take the default value of t3.micro
   
 }
