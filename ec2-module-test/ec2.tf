98f2939fa65921301a92ece4b2f1a055739f77fe
Balaji <balaji@eviridis.com>
2026-03-17 18:21:43 +0530

    notes updated

--- ec2-module-test/ec2.tf
+++ ec2-module-test/ec2.tf
@@ -1,5 +1,11 @@
 module "ec2" {  
     source = "../terraform-aws-instance"
+    environment = "dev"
+    project = "roboshop"
+    sg_ids = ["sg-085cf1fdfcefdb4c0"]
     ami_id = "ami-0220d79f3f480ecf5"
     instance_type = "t3.large"
+    tags = {
+        name = "roboshop-dev"
+    }
 } 
--- terraform-aws-instance/ec2.tf
+++ terraform-aws-instance/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.sg_ids
