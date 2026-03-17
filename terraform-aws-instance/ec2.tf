ba79a19a8ae784fe610a923b4352e123abf40f88
Balaji <balaji@eviridis.com>
2026-03-17 18:08:25 +0530

    notes updated

--- terraform-aws-instance/ec2.tf
+++ terraform-aws-instance/ec2.tf
@@ -1,35 +1,7 @@
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = var.sg_ids
 
-  tags = {
-    Name = "terraform"
-    Project = "roboshop"
-  }
-}
-
-resource "aws_security_group" "allow_tls" {  #this is for terraform
-  name        = "allow-all-terraform" # this is for AWS account
-  description = "Allow TLS inbound traffic and all outbound traffic"
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow-all-terraform"
-  }
+  tags = local.ec2_final_tags
 }
--- terraform-aws-instance/variables.tf
+++ terraform-aws-instance/variables.tf
@@ -1,3 +1,12 @@
+variable "project" {
+    type = string
+  
+}
+variable "environment" {
+    type = string
+  
+}
+
 variable "ami_id" { #Here Ami_id is mandatory variable, we have to pass the value of this variable when we are calling this module in other terraform file. This is for making our code reusable and modular. We can use this module for creating multiple ec2 instances with different ami_id and instance_type by just passing the values of these variables.
     type = string
     
@@ -6,5 +15,16 @@ variable "ami_id" { #Here Ami_id is mandatory variable, we have to pass the valu
 variable "instance_type" { 
     type = string
     default = "t3.micro" # this is for default value, if we are not passing the value of this variable when we are calling this module in other terraform file, then it will take the default value of t3.micro
+  
+}
+
+variable "sg_ids" {
+    type = list(string)
+  
+}
+
+variable "tags" {
+    type = map
+    default = {}  #this is optional variable, if we are not passing the value of this variable when we are calling this module in other terraform file, then it will take the default value of empty map
   
 }
