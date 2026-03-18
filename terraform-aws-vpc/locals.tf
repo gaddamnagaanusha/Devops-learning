715d2422525a8be03b052024b611a1fbf0ee87d0
Balaji <balaji@eviridis.com>
2026-03-18 17:06:39 +0530

    notes updated

---
+++ terraform-aws-vpc/locals.tf
@@ -0,0 +1,16 @@
+locals {
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    terraform   = "true"
+  }
+
+  vpc_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"  #this is for giving the name to our vpc, we are using the merge function to merge the common_tags with the name tag, so that we can have the name tag in our vpc along with the common tags
+        },
+
+        var.vpc_tags
+    )
+}
---
+++ terraform-aws-vpc/variables.tf
@@ -0,0 +1,22 @@
+variable "project" {
+    type = string
+  
+}
+
+variable "environment" {
+   type = string
+
+}
+
+
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+  
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {}  #this is optional variable, if we are not passing the value of this variable when we are calling this module in other terraform file, then it will take the default value of empty map
+  
+}
