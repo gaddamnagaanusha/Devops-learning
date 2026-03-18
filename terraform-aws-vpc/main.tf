f3f4a931d9a8122fb1c8dc8b3baed7743b18f5d4
Balaji <balaji@eviridis.com>
2026-03-18 17:57:11 +0530

    notes updated

--- terraform-aws-vpc/main.tf
+++ terraform-aws-vpc/main.tf
@@ -10,7 +10,5 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id  #vpc associated with this internet gateway
 
-  tags = {
-    Name = local.igw_final_tags
-  }
+  tags = local.igw_final_tags
 }
