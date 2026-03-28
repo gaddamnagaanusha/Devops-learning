6412a9d465cd17a53f89cf6eb1acd94c5bca2c36
Balaji <balaji@eviridis.com>
2026-03-28 15:53:34 +0530

    notes updated

--- roboshop-infra-dev/10-sg/main.tf
+++ roboshop-infra-dev/10-sg/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
     source = "../../terraform-aws-sg"
-    project = "roboshop"
-    environment = "dev"
+    project = var.project
+    environment = var.environment
     sg_name =  "mongodb"
-    vpc_id = module.vpc.vpc_id
+    vpc_id = local.vpc_id
 
 }
