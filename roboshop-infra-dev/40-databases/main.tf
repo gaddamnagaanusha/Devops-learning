d47e5bba73fbbea449a8c0a102b0ca24a76e0ecd
Balaji <balaji@eviridis.com>
2026-03-28 21:35:51 +0530

    notes updated

--- roboshop-infra-dev/40-databases/main.tf
+++ roboshop-infra-dev/40-databases/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "bastion" {
+resource "aws_instance" "mongodb" {
   ami           = local.ami_id
   instance_type = "t3.micro"
   subnet_id = local.database_subnet_ids
@@ -12,4 +12,29 @@ resource "aws_instance" "bastion" {
     local.common_tags,
   )
 
+}
+
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mongodb.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh"    # Destination path on the remote machine
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
+    ]
+  }
 }
