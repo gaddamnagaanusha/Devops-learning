58c3823ed0fede1e5e146a9e52f5f43769436316
Balaji <balaji@eviridis.com>
2026-03-28 21:10:10 +0530

    notes updated

--- roboshop-infra-dev/40-databases/data.tf
+++ roboshop-infra-dev/40-databases/data.tf
@@ -18,10 +18,10 @@ data "aws_ami" "devops" {
       }
 }
 
-data "aws_ssm_parameter" "public_subnet_ids" {
-    name = "/${var.project}/${var.environment}/public_subnet_ids"
+data "aws_ssm_parameter" "database_subnet_ids" {
+    name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
 
-data "aws_ssm_parameter" "bastion_sg_id" {
-    name = "/${var.project}/${var.environment}/bastion_sg_id"
+data "aws_ssm_parameter" "mongodb_sg_id" {
+    name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
--- roboshop-infra-dev/40-databases/locals.tf
+++ roboshop-infra-dev/40-databases/locals.tf
@@ -6,8 +6,8 @@ locals {
     }
   ami_id = data.aws_ami.devops.id
   #public subnet in 1a AZ
-  public_subnet_ids = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0]
-  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+  database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
 
   
