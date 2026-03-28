1606d1f9f8376bfc1ef7822ff2a0251cf9e6d1a9
Balaji <balaji@eviridis.com>
2026-03-28 16:52:53 +0530

    notes updated

--- roboshop-infra-dev/00-vpc/output.tf
+++ roboshop-infra-dev/00-vpc/output.tf
@@ -7,3 +7,13 @@ output "public_subnet_ids" {
     value = module.vpc.public_subnet_ids
   
 }
+
+output "private_subnet_ids" {
+    value = module.vpc.private_subnet_ids
+  
+}
+
+output "database_subnet_ids" {
+    value = module.vpc.database_subnet_ids
+  
+}
