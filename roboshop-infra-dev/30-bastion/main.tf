83bc52d6e93a84f6747c8aa058325ef32089e20c
Balaji <balaji@eviridis.com>
2026-03-28 22:09:36 +0530

    notes updated

--- roboshop-infra-dev/30-bastion/main.tf
+++ roboshop-infra-dev/30-bastion/main.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "bastion" {
   subnet_id = local.public_subnet_ids
   vpc_security_group_ids = [local.bastion_sg_id]
   iam_instance_profile = aws_iam_instance_profile.bastion.name
-  user_data = file(bastion.sh)
+  user_data = file("bastion.sh")
+
   root_block_device {  #increasing root block device configuration 
      volume_size = 50
      volume_type = "gp3"
