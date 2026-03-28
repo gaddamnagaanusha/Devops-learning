313d06f647ab130c9b65c63789e39db6586666fe
Balaji <balaji@eviridis.com>
2026-03-28 18:01:20 +0530

    notes updated

--- roboshop-infra-dev/20-sg-rules/datasource.tf
+++ roboshop-infra-dev/20-sg-rules/datasource.tf
@@ -13,3 +13,7 @@ data "aws_ssm_parameter" "mongodb_sg_id" {
 data "aws_ssm_parameter" "catalogue_sg_id" {
     name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
+
+data "aws_ssm_parameter" "user_sg_id" {
+    name = "/${var.project}/${var.environment}/user_sg_id"
+}
--- roboshop-infra-dev/20-sg-rules/main.tf
+++ roboshop-infra-dev/20-sg-rules/main.tf
@@ -34,3 +34,15 @@ resource "aws_security_group_rule" "mongodb_catalogue" {
 
   security_group_id = local.mongodb_sg_id
 }
+
+resource "aws_security_group_rule" "mongodb_user" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+   
+   #where traffic is coming from 
+  source_security_group_id = local.user_sg_id
+
+  security_group_id = local.mongodb_sg_id
+}
