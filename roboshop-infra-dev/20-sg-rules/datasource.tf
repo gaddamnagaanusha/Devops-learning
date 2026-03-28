5dc5ae321e5ee2db5a1bb09fb75ad9a8a83abfff
Balaji <balaji@eviridis.com>
2026-03-28 17:57:23 +0530

    notes updated

--- roboshop-infra-dev/20-sg-rules/datasource.tf
+++ roboshop-infra-dev/20-sg-rules/datasource.tf
@@ -9,3 +9,7 @@ data "aws_ssm_parameter" "bastion_sg_id" {
 data "aws_ssm_parameter" "mongodb_sg_id" {
     name = "/${var.project}/${var.environment}/mongodb_sg_id"
 }
+
+data "aws_ssm_parameter" "catalogue_sg_id" {
+    name = "/${var.project}/${var.environment}/catalogue_sg_id"
+}
--- roboshop-infra-dev/20-sg-rules/main.tf
+++ roboshop-infra-dev/20-sg-rules/main.tf
@@ -22,3 +22,15 @@ resource "aws_security_group_rule" "mongodb_bastion" {
 
   security_group_id = local.mongodb_sg_id
 }
+
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+   
+   #where traffic is coming from 
+  source_security_group_id = local.catalogue_sg_id
+
+  security_group_id = local.mongodb_sg_id
+}
