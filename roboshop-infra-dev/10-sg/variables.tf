3693892d009afa7ce54fe6796fa17fe2b4224884
Balaji <balaji@eviridis.com>
2026-03-28 17:07:12 +0530

    notes updated

--- roboshop-infra-dev/10-sg/variables.tf
+++ roboshop-infra-dev/10-sg/variables.tf
@@ -7,3 +7,27 @@ variable "environment" {
     default = "dev"
   
 }
+
+variable "sg_names" {
+    type = list
+    default = [
+       #databases
+        "mongodb","redis","mysql","rabbitmq",
+
+       #backend
+        "catalogue","cart","user","payment","shipping",
+
+        #backend ALB
+        "backend_alb",
+
+       #frontend
+        "frontend",
+       #frontend ALB
+        "frontend_alb",
+
+        #bastion
+        "bastion"
+]
+    
+  
+}
